46cfd8ea747ff5d1fff7a2745af1dc5fee6c22d1
Peter Szabo <pts@fazekas.hu>
2014-12-06 11:10:34 +0100

    added some files from http://pcc.ludd.ltu.se/ftp/pub/pcc-releases/pcc-1.1.0.tgz (2014-12-06)

---
+++ mip/unicode.h
@@ -0,0 +1,9 @@
+/*	$Id: unicode.h,v 1.4 2014/08/22 13:13:29 plunky Exp $	*/
+
+#ifndef _UNICODE_H
+#define _UNICODE_H
+
+extern unsigned int u82cp(char **q);
+extern void u8error(const char *fmt, ...);
+
+#endif
