49a41a75c4a6e9150f2a069ae2b5a274047ec720
Peter Szabo <pts@fazekas.hu>
2023-06-10 20:04:03 +0200

    refactor: added CFLAGS to control -DPCC_DEBUG

--- compile.sh
+++ compile.sh
@@ -3,12 +3,12 @@
 # compile.sh: build script for PCC 1.1.0 on Linux, targeting Linux i386
 # by pts@fazekas.hu at Sat Jun 10 12:46:04 CEST 2023
 #
-# Compile with: ./compile.sh gcc   -s -Os -W -Wall -Wmissing-prototypes -Wshadow -Wsign-compare -Wno-unused-parameter -Werror-implicit-function-declaration -std=c99 -pedantic -Wno-format -Wno-format-pedantic
-# Compile with: ./compile.sh clang -s -Os -W -Wall -Wmissing-prototypes -Wshadow -Wsign-compare -Wno-unused-parameter -Werror-implicit-function-declaration -std=c99 -pedantic -Wno-format -Wno-format-pedantic
-# Compile with: ./compile.sh owcc -blinux -march=i386 -s -O2 -I"$WATCOM"/lh -fsigned-char -fno-stack-protector -W -Wextra -Wno-n303 -std=c99
-# Compile with: ./compile.sh minicc --gcc --diet -Wno-unused-parameter -std=c99 -pedantic -Wno-format -Wno-format-pedantic
-# Compile with: ./compile.sh minicc --wcc --diet -Wno-unused-parameter -std=c99 -pedantic -Wno-format -Wno-format-pedantic
-# Compile with: ./compile.sh minicc --tcc --diet -Wno-unused-parameter -std=c99 -pedantic -Wno-format -Wno-format-pedantic
+# Compile with: ./compile.sh gcc   -s -Os -W -Wall -Wmissing-prototypes -Wshadow -Wsign-compare -Wno-unused-parameter -Werror-implicit-function-declaration -std=c99 -pedantic
+# Compile with: ./compile.sh clang -s -Os -W -Wall -Wmissing-prototypes -Wshadow -Wsign-compare -Wno-unused-parameter -Werror-implicit-function-declaration -std=c99 -pedantic
+# Compile with: ./compile.sh owcc -blinux -march=i386 -s -O2 -I"$WATCOM"/lh -fsigned-char -fno-stack-protector -W -Wextra -Wno-n303 -std=c99 -fo=.obj && -f *.obj
+# Compile with: ./compile.sh minicc --gcc --diet -Wno-unused-parameter -std=c99 -pedantic
+# Compile with: ./compile.sh minicc --wcc --diet -Wno-unused-parameter -std=c99 -pedantic
+# Compile with: ./compile.sh minicc --tcc --diet -Wno-unused-parameter -std=c99 -pedantic
 #
 
 set -ex
@@ -18,18 +18,20 @@ set -ex
 CC='gcc -s -Os -W -Wall -Wmissing-prototypes -Wshadow -Wsign-compare'
 test $# = 0 || CC="$*"
 I386_CCLD="${I386_CCLD:-gcc -m32}"
+#CFLAGS=''  # -DPCC_DEBUG
+CFLAGS='-DPCC_DEBUG'
 
 test -d pccbin || mkdir pccbin
-$CC -DLIBEXECDIR=\"/usr/local/libexec/\" -DGCC_COMPAT -DPCC_DEBUG -DINCLUDEDIR=\"/usr/local/h/\" -DPCCINCDIR=\"/usr/local/lib/pcc/i386-pc-linux-gnu/1.1.0/h/\" -DPCCLIBDIR=\"/usr/local/lib/pcc/i386-pc-linux-gnu/1.1.0/lib/\" -Dos_linux -Dmach_i386 -DTARGOSVER=0 -DCPPROGNAME=\"pcpp\" \
+$CC -DLIBEXECDIR=\"/usr/local/libexec/\" -DGCC_COMPAT $CFLAGS -DINCLUDEDIR=\"/usr/local/h/\" -DPCCINCDIR=\"/usr/local/lib/pcc/i386-pc-linux-gnu/1.1.0/h/\" -DPCCLIBDIR=\"/usr/local/lib/pcc/i386-pc-linux-gnu/1.1.0/lib/\" -Dos_linux -Dmach_i386 -DTARGOSVER=0 -DCPPROGNAME=\"pcpp\" \
     -Ih/cc/driver -Ih/top -Ih/os/linux -Ih/mip -Ih/arch/i386 \
     src/cc/cc/cc.c src/mip/compat.c src/cc/driver/strlist.c src/cc/driver/xalloc.c src/bsd/strlcpy.c src/bsd/strlcat.c \
     -o pccbin/pcc
-$CC -DGCC_COMPAT -DPCC_DEBUG -D_ISOC99_SOURCE -Dos_linux -Dmach_i386 \
+$CC -DGCC_COMPAT $CFLAGS -D_ISOC99_SOURCE -Dos_linux -Dmach_i386 \
    -Ih/cc/ccom -Ih/top -Ih/mip -Ih/arch/i386 -Ih/os/linux \
     src/cc/ccom/builtins.c src/cc/ccom/cgram_auto.c src/arch/i386/code.c src/mip/common.c src/mip/compat.c src/cc/ccom/external_auto.c src/cc/ccom/gcc_compat.c src/cc/ccom/init.c src/cc/ccom/inline.c src/arch/i386/local.c src/arch/i386/local2.c src/cc/ccom/main.c \
     src/mip/match.c src/cc/ccom/optim.c src/mip/optim2.c src/arch/i386/order.c src/cc/ccom/pftn.c src/mip/reader.c src/mip/regs.c src/cc/ccom/scan_auto.c src/cc/ccom/stabs.c src/cc/ccom/symtabs.c src/arch/i386/table.c src/cc/ccom/trees.c src/mip/unicode.c src/bsd/strlcpy.c src/bsd/strlcat.c \
     -o pccbin/ccom
-$CC -DGCC_COMPAT -DPCC_DEBUG -D_ISOC99_SOURCE -Dos_linux -Dmach_i386 \
+$CC -DGCC_COMPAT $CFLAGS -D_ISOC99_SOURCE -Dos_linux -Dmach_i386 \
     -Ih/cc/cpp -Ih/top -Ih/mip -Ih/arch/i386 \
     src/mip/compat.c src/cc/cpp/cpp.c src/cc/cpp/cpy_auto.c src/cc/cpp/token.c src/bsd/strlcpy.c src/bsd/strlcat.c \
     -o pccbin/cpp
